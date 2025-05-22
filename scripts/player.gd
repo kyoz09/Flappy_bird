c6e2cd1510e06ae2b16f447f4bd9d5c63784461d
kyoz <fatimarifay0312@gmail.com>
2025-05-22 19:18:56 -0400

    add player scene

---
+++ scripts/player.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+var speed = 300
+var score = 0
+
+func _ready() -> void:
+	var viewport_size = get_viewport().get_visible_rect().size
+	position = viewport_size/2
+	
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if Input.is_action_just_pressed("volar"):
+		linear_velocity = Vector2.ZERO
+		apply_central_impulse(Vector2(0, -speed))
+		$AnimatedSprite2D.play("up")
+		
